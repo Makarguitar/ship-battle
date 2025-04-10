6dc228478b9e94c5364af7b8e54be6760e6739e9
makar <makmakhorin@hotmail.com>
2025-04-10 17:54:49 +0400

    game in progress

---
+++ BattleField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace morskoy_boy
+{
+    class BattleField
+    {
+        string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+
+        char[,] visibleArea = new char[10, 10];
+    }
+}
