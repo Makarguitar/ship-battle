66c2ffcaae17ba01cab6d34425758a6a7b3dc790
makar <makmakhorin@hotmail.com>
2025-04-10 17:39:19 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace morskoy_boy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
